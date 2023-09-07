b76dd9616f55d29a0363e79f900f652f21af69e2
joker-jonesy <spashmcgrath94@yahoo.com>
2023-09-07 12:43:29 -0400

    kermit

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,25 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
+import {Provider} from "react-redux";
+import store from "./store.js";
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+import PokemonPage from "./pages/pokemon/PokemonPage.jsx";
+import SinglePokemonPage from "./pages/spokemon/SinglePokemonPage.jsx";
+import CartPage from "./pages/cart/CartPage.jsx";
+import Nav from "./components/Nav.jsx";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Provider store={store}>
+          <Nav/>
+        <Routes>
+          <Route path={"/"} element={<PokemonPage/>}/>
+          <Route path={"/cart"} element={<CartPage/>}/>
+          <Route path={"/pokemon/:id"} element={<SinglePokemonPage/>}/>
+        </Routes>
+      </Provider>
+    </BrowserRouter>
   </React.StrictMode>,
 )
